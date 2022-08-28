efcb51f9f1d28ce56f5dfe75a5f9ae7e8d572295
zeeunltd <azegaf@gmail.com>
2022-08-28 19:25:54 +0100

    Clearing issues

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,15 @@
+// const startApp = async (any) => {
+//   const list = await fetch(
+//     "https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84"
+//   ).then((result) => result.json());
+//   console.log({ list });
+// };
 
-const startApp = async () => {
-    const list = await fetch('https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84')
-    .then(
-        result => result.json()
-    )
-    console.log({list});
-};
+// const fetchList = async (page:any) => {
+//   const list:  = await fetch(
+//     "https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84"
+//   ).then((result) => result.json());
+//   return { list, page };
+// };
 
-const fetchList = async ( page) => {
-    const list = await fetch('https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84')
-    .then(
-        result => result.json()
-    )
-    return {list, page}
-}
-
-export function laodNext () {
-    console.log('isLoading');
-    return 'done '
-}
-document.addEventListener('DOMContentLoaded', startApp);
+// document.addEventListener("DOMContentLoaded", startApp);
