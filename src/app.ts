54489842877aa0531ffdde5403a8e76ed7745e7c
zeeunltd <azegaf@gmail.com>
2022-08-28 19:05:41 +0100

    Update - Completed configurations - Setup Netlify Configurations - Deployed to Netlify - Fetch Data and save

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
 
 const startApp = async () => {
-    
+    const list = await fetch('https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84')
+    .then(
+        result => result.json()
+    )
+    console.log({list});
 };
 
+const fetchList = async ( page) => {
+    const list = await fetch('https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84')
+    .then(
+        result => result.json()
+    )
+    return {list, page}
+}
+
+export function laodNext () {
+    console.log('isLoading');
+    return 'done '
+}
 document.addEventListener('DOMContentLoaded', startApp);
