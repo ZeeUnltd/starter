9c6cd603879578ca7a39840389dc52384fd8eb91
zeeunltd <azegaf@gmail.com>
2022-08-29 21:28:08 +0100

    Update - Fixed issue with table data not populating

--- src/app.js
+++ src/app.js
@@ -1,51 +1,67 @@
-let list = [];
-// const tableBody = document.querySelector("tableBody")
+const prevBtn = document.getElementById("prevBtn")
+const nextBtn = document.getElementById("nextBtn")
+const pageView = document.getElementById("pageView")
+
+let list = null;
+let currentPage;
+let paging;
+let isLoading;
 
 const startApp = async () => {
-  const result = await fetchList();
-  if (result) {
-    list = result;
-    populateTable();
-  }
+    const result = await fetchList();
+    if (result) {
+        list = result;
+        return  populateTable();
+    }
 };
 
-const fetchList = async (page = 1) => {
-  const list = await fetch(
-    "https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84" +
-      `&page=${page}`
-  ).then((result) => result.json());
-  return list;
+ async function fetchList(page = 1) {
+    const response = await fetch(
+        "https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84" +
+        `&page=${page}`
+    ).then((result) => result.json());
+    currentPage = response.info.page
+    paging = response.results[0].paging
+    
+    pageView.innerText = `Showing Page ${currentPage}`
+    list = response
+    populateTable(currentPage)
 };
 
-function populateTable() {
-  const table = document.getElementById("tableBody");
-  const male = list.results[0][2];
-  const female = list.results[0][2];
+async function populateTable(n = 1) {
+    let wholeTable = document.getElementById("mainTable");
+    let newTableBody = document.createElement('tbody')
+    let pageData = list.results[0][`${n}`]
 
-  console.log({ list, male, female });
-  //   list.results[0][0].forEach((item, index) => {
-  //     // tr = document.createElement("tr")
-  //     // td = document.createElement("td")
+    await pageData.forEach((item, index) => {
 
-  //     // tr.append(td)
-  //     // tableBody.append(tr)
+        const row = newTableBody.insertRow(-1)
+        row.setAttribute('data-entryid', item.id)
 
-  //     // Create an empty <tr> element and add it to the 1st position of the table:
-  //     const row = table.insertRow(-1)
+        // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+        const cell1 = row.insertCell(0)
+        const cell2 = row.insertCell(1)
+        const cell3 = row.insertCell(1)
 
-  //     // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-  //     const cell1 = row.insertCell(0)
-  //     const cell2 = row.insertCell(1)
-  //     const cell3 = row.insertCell(1)
-
-  //     // Add some text to the new cells:
-  //     cell1.innerHTML = `${index+1}`
-  //     cell2.innerHTML = `${item}`
-  //     cell3.innerHTML = `${item}`
-  //   })
+        // Add some text to the new <td></td>elements:
+        cell1.innerHTML = `${index + 1}`
+        cell2.innerHTML = `${item.gender}`
+        cell3.innerHTML = `${item.age}`
+        return  wholeTable.lastElementChild.replaceWith(newTableBody)
+        
+    })
 }
-function laodNext(data) {
-  console.log("isLoading");
-  return "done ";
-}
+
+prevBtn.addEventListener('click', () => {
+    debugger
+    const N = parseInt(currentPage) -1 
+    console.log({N}, ':1o:');
+    return !paging?.previous ? '' : fetchList(N)
+})
+nextBtn.addEventListener('click', () => {
+    const N = parseInt(currentPage) + 1
+    console.log({N}, ':o1:');
+    return !paging?.next ? '' : fetchList(N)
+})
+
 document.addEventListener("DOMContentLoaded", startApp);
